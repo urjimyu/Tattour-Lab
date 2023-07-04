7f5ab3f74b6a5efa9ea5bbb3ded3c8f5f02bfdfe
jimyu <aliyah_7@naver.com>
2023-07-05 05:19:08 +0900

    feat: 전체 삭제 구현중

--- tattour-lab/src/Canvas.tsx
+++ tattour-lab/src/Canvas.tsx
@@ -1,40 +1,49 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { fabric } from "fabric";
 import ColorPicker from "./ColorPicker";
+import styled from "styled-components";
 
 const Canvas: React.FC = () => {
-	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+	const canvasRef = useRef<HTMLCanvasElement>(null);
 	const fabricCanvasRef = useRef<fabric.Canvas | null>(null);
+	const [canvas, setCanvas] = useState<fabric.Canvas | null>(null);
+
+	const initCanvas = () =>
+		new fabric.Canvas(canvasRef.current, {
+			isDrawingMode: true,
+			width: 335,
+			height: 458,
+		});
 
 	useEffect(() => {
-		const canvas = new fabric.Canvas(canvasRef.current!, {
-			isDrawingMode: true, // 그리기 모드 설정
-		});
+		setCanvas(initCanvas());
+		if (canvas) {
+			// 그리기 설정
+			canvas.freeDrawingBrush.color = "black";
+			canvas.freeDrawingBrush.width = 5;
 
-		// 그리기 설정
-		canvas.freeDrawingBrush.color = "black";
-		canvas.freeDrawingBrush.width = 5;
+			// 그리기 모드에서 스크롤 방지
+			canvas.on("mouse:wheel", (event: fabric.IEvent) => {
+				const delta = (event as any).e.deltaY;
+				const zoom = canvas.getZoom();
+				if (delta > 0) {
+					canvas.setZoom(zoom * 1.1);
+				} else {
+					canvas.setZoom(zoom * 0.9);
+				}
+				event.e.preventDefault();
+				event.e.stopPropagation();
+			});
 
-		// 그리기 모드에서 스크롤 방지
-		canvas.on("mouse:wheel", (event: fabric.IEvent) => {
-			const delta = (event as any).e.deltaY;
-			const zoom = canvas.getZoom();
-			if (delta > 0) {
-				canvas.setZoom(zoom * 1.1);
-			} else {
-				canvas.setZoom(zoom * 0.9);
-			}
-			event.e.preventDefault();
-			event.e.stopPropagation();
-		});
+			fabricCanvasRef.current = canvas;
 
-		fabricCanvasRef.current = canvas;
-
-		//캔버스 지우기
-		return () => {
-			canvas.dispose();
-		};
+			return () => {
+				canvas.dispose();
+			};
+		}
 	}, []);
+
+	// useEffect(() => {}, [canvas]);
 
 	//색상 변경
 	const handleColorChange = (color: string) => {
@@ -43,12 +52,27 @@ const Canvas: React.FC = () => {
 		}
 	};
 
+	const handleDelete = () => {
+		canvas?.dispose();
+	};
+
 	return (
 		<div>
 			<canvas ref={canvasRef} />
 			<ColorPicker onChange={handleColorChange} />
+			<St.Button type="button" value="삭제" onClick={handleDelete}>
+				전체 삭제
+			</St.Button>
 		</div>
 	);
 };
 
 export default Canvas;
+
+const St = {
+	Button: styled.button`
+		color: white;
+		font-size: 1rem;
+		background-color: hotpink;
+	`,
+};
